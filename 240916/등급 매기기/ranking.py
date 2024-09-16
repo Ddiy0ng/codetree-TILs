ad2ccc2517190a0dd8c0011efa81cb25a02c131e
Ddiy0ng <bueong0627@hufs.ac.kr>
2024-09-16 13:58:14 +0900

    Title: 등급 매기기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240916/등급 매기기/ranking.py
+++ 240916/등급 매기기/ranking.py
@@ -1,11 +1,11 @@
 s = int(input())
-if s >= 90
+if s >= 90:
     print("A")
-elif s >= 80
+elif s >= 80:
     print("B")
-elif s >= 70
+elif s >= 70:
     print("C")
-elif s >= 60
+elif s >= 60:
     print("D")
-else
+else:
     print("F")
