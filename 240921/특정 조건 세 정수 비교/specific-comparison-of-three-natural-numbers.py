2f5967f9138d58c1d96832f5a86208ac2a243a88
Ddiy0ng <bueong0627@hufs.ac.kr>
2024-09-21 20:26:21 +0900

    Title: 특정 조건 세 정수 비교, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240921/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,15 @@
+a = input()
+arr = a.split()
+n1 = arr[0]
+n2 = arr[1]
+n3 = arr[2]
+
+if n1 < n2 and n1 < n3:
+    a1 = 1
+else:
+    a1 = 0
+if n1 == n2 and n2 == n3:
+    a2 = 1
+else:
+    a2 = 0
+print(a1, a2)
