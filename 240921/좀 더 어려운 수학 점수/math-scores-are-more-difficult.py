9653e0d69d14548a848d777b0c6e90b42b5e07f4
Ddiy0ng <bueong0627@hufs.ac.kr>
2024-09-21 20:52:09 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240921/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,11 @@
+mA, eA = map(int, input().split())
+mB, eA = map(int, input().split())
+if mA > mB:
+    print(A)
+elif mA == mB:
+    if eA > eB:
+        print('A')
+    else:
+        print('B')
+else:
+    print('B')
